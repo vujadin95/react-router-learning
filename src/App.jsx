876088113c914138fef255ab06e833add57c06b7
vujadin95 <vujke456@gmail.com>
2024-08-29 15:13:34 +0200

    initializing repository on github for react router dom v6.4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Starting my learning journey with react router dom v6.4</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
